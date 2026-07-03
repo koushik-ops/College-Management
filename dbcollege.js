0c4f48c3022f2562949861a30f284ea327727063
KOUSHIK DEB <koushikofficial1113@gmail.com>
2026-07-03 14:28:06 +0530

    Update database password in dbcollege.js

--- dbcollege.js
+++ dbcollege.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "deepa",
+    password: "#Koushik460",
     // database: "college2"
 });
 
